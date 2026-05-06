497b62d857f29a9d48179c30e4833c179960d769
aryangosw1234-arch <aryangosw1234@gmail.com>
2026-05-06 19:51:24 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = "todo-app-v2";
+const CACHE_NAME = "todo-app-v3";
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting(); // IMPORTANT
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
@@ -8,9 +9,20 @@ self.addEventListener("install", (event) => {
         "/todo-app/index.html",
         "/todo-app/style.css",
         "/todo-app/script.js",
-        "/todo-app/manifest.json",
         "/todo-app/icon.png"
       ]);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim()); // IMPORTANT
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
